2370e5b84dff2491d2f6a79c86ff3eeddd501917
cjackson914 <63318622+cjackson914@users.noreply.github.com>
2020-11-13 23:12:14 -0500

    mountain animation started

--- dev/js/mountainanimation.js
+++ dev/js/mountainanimation.js
@@ -8,9 +8,26 @@ const mountainTL = gsap.timeline();
 
 export function mountainAnimation(){
 
-  mountainTL.to("#sky",{duration:1, fill:"#3a6f84"},"play")
+  mountainTL.from("#Bkg",{duration:.5, fill:"#1D2A3F"})
+  .from("#Mtn6",{duration:.5, fill:"#1D2A3F"})
+  .from("#Mtn5",{duration:.5, fill:"#1D2A3F"})
+  .from("#Mtn4",{duration:.5, fill:"#1D2A3F"})
+  .from("#JeepLogoBack",{duration:.5, y:50, alpha:0})
+  .from("#Mtn3",{duration:.5, fill:"#1D2A3F"},"Mtn3")
+  .to("#Mtn3",{duration:.5, alpha:0},"Mtn3")
+  .from("#Mtn2",{duration:.5, fill:"#1D2A3F"})
+  .from("#Mtn1",{duration:.5, fill:"#1D2A3F"})
+  .from("#JeepLogoFront",{duration:.5, alpha:0})
 
+  .to("#Mtn1",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"})
+  .to("#Mtn2",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"})
 
+  // .to("#Mtn3",{duration:.5, fill:"#1D2A3F"})
+  .to("#Mtn35",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"})
+  .to("#Mtn4",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"})
+  .to("#Mtn5",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"})
+  .to("#Mtn6",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"})
+  .to("#Bkg",{duration:.5, fill:"#1D2A3F", stroke:"#1D2A3F"});
 
-  return mountainAnimation;
+  // return mountainAnimation;
 }
